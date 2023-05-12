f220d367caee7ca0266824b351eb8ed0087d0fdb
chenyungui <286224043@qq.com>
2023-05-12 09:24:13 +0800

    initial commit

--- TestWholeModule/ViewController.swift
+++ TestWholeModule/ViewController.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import WholeModule
 
 class ViewController: UIViewController {
 
+    var path: CVPath!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
 }
 
